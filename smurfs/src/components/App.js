c260916c960f2c296743628f65eeedfd6a291e48
christiansallen <christiansallen@gmail.com>
2019-02-01 10:08:15 -0700

    Add smurf functionality complete

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -10,14 +10,73 @@ import { connect } from "react-redux";
 import { getSmurfs, addSmurf } from "../actions";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      age: "",
+      height: ""
+    };
+  }
+
   componentDidMount() {
     this.props.getSmurfs();
   }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    };
+    this.props.addSmurf(newSmurf);
+    this.setState({
+      name: "",
+      age: "",
+      height: ""
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Smurf Village!</h1>
+
+        <form className="add-smurf-form" onSubmit={this.onSubmit}>
+          <input
+            className="input"
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={this.state.name}
+            onChange={this.onChange}
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="Age"
+            name="age"
+            value={this.state.age}
+            onChange={this.onChange}
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="Height"
+            name="height"
+            value={this.state.height}
+            onChange={this.onChange}
+          />
+          <button onClick={this.onSubmit}>Add Smurf!</button>
+        </form>
+
         <span className="smurfs-container">
           {this.props.smurfs.map(smurf => {
             return (
