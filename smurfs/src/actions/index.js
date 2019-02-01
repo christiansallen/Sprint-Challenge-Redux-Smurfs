baad6d238bd7652a5997adce23000ad50e934760
christiansallen <christiansallen@gmail.com>
2019-02-01 10:23:05 -0700

    Delete Smurf functionality complete

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -8,6 +8,10 @@ export const ADD_SMURF_START = "ADD_SMURF_START";
 export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESS";
 export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE";
 
+export const DELETE_SMURF_START = "DELETE_SMURF_START";
+export const DELETE_SMURF_SUCCESS = "DELETE_SMURF_SUCCESS";
+export const DELETE_SMURF_FAILURE = "DELETE_SMURF_FAILURE";
+
 export const getSmurfs = () => dispatch => {
   dispatch({ type: GET_SMURFS_START });
   axios
@@ -24,13 +28,10 @@ export const addSmurf = smurf => dispatch => {
     .catch(err => dispatch({ type: ADD_SMURF_FAILURE, payload: err }));
 };
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const deleteSmurf = id => dispatch => {
+  dispatch({ type: DELETE_SMURF_START });
+  axios
+    .delete(`http://localhost:3333/smurfs/${id}`)
+    .then(res => dispatch({ type: DELETE_SMURF_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: DELETE_SMURF_FAILURE, payload: err }));
+};
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -4,7 +4,10 @@ import {
   GET_SMURFS_FAILURE,
   ADD_SMURF_START,
   ADD_SMURF_SUCCESS,
-  ADD_SMURF_FAILURE
+  ADD_SMURF_FAILURE,
+  DELETE_SMURF_START,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_FAILURE
 } from "../actions";
 
 const initialState = {
@@ -35,6 +38,7 @@ const smurfReducer = (state = initialState, action) => {
         fetchingSmurfs: false,
         error: action.payload
       };
+
     case ADD_SMURF_START:
       return {
         ...state,
@@ -50,6 +54,24 @@ const smurfReducer = (state = initialState, action) => {
       return {
         ...state,
         addingSmurf: false,
+        error: action.payload
+      };
+
+    case DELETE_SMURF_START:
+      return {
+        ...state,
+        deletingSmurf: true
+      };
+    case DELETE_SMURF_SUCCESS:
+      return {
+        ...state,
+        deletingSmurf: false,
+        smurfs: action.payload
+      };
+    case DELETE_SMURF_FAILURE:
+      return {
+        ...state,
+        deletingSmurf: false,
         error: action.payload
       };
     default:
